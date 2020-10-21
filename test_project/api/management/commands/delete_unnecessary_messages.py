5b074591fcf3be94361329fd37fa8064a139932a
Paweł <pawel1830@gmail.com>
2020-10-21 10:35:00 +0200

    older than 5 minutes

--- test_project/api/management/commands/delete_unnecessary_messages.py
+++ test_project/api/management/commands/delete_unnecessary_messages.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.core.management.base import BaseCommand
 
 from test_project.api.models import Message
@@ -9,5 +9,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         now = datetime.now()
-        message_uuids = [message['uuid'] for message in Message.objects.filter(created_at__gte=now)]
+        query = Message.objects.filter(created_at__gte=now-timedelta(minutes=5))
+        message_uuids = [message['uuid'] for message in query]
         Message.objects.filter(uuid__in=message_uuids).delete()
